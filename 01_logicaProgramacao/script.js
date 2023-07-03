cf2464cb3b176a1f63e2f28d809a42dbb542e76a
mewmewAwayHome <mewmewdevart@gmail.com>
2023-07-03 16:57:26 -0300

    scripts(content) : exercicio da logica de programacao basica

---
+++ 01_logicaProgramacao/script.js
@@ -0,0 +1,21 @@
+console.log("Olá, tudo bem?");
+console.warn("Toma cuidado!");
+console.error("Deu ruim...:(");
+
+// number (int or float)
+var student_grade = 9.5; // inicializando
+console.log(student_grade);
+student_grade = 10.5; // atribuindo valor à variavel
+
+var numberLucas; // declarando a variavel
+numberLucas = numberLucas + 1; // atribuicao
+numberLucas += 1;
+numberLucas++;
+
+var teste = 1;
+teste++;
+console.log(teste);
+
+teste = 1;
+++teste;
+console.log(teste);
