06e6f83d4438d6b2d54dbefe266653d6795249d3
Larissa-Start <larih171099@gmail.com>
2023-08-04 16:30:21 -0300

    scripts(content) : footer + detalhes de details

--- 02_FrameworkAngular_I/QuartoAndar/src/app/rodape/rodape.component.ts
+++ 02_FrameworkAngular_I/QuartoAndar/src/app/rodape/rodape.component.ts
@@ -7,25 +7,24 @@ import { Component } from '@angular/core';
 })
 
 export class RodapeComponent {
-  title: string = 'Lar Feliz Imóveis';
+  title: string = 'QuartoAndar';
   copyright: string = 'Desenvolvido em 2023';
   developer: string = 'Start Tech TOTVS';
 
   redesSociais: Array<any> = [
       {
-        nome: 'Facebook',
-        link: 'http://www.facebook.com',
-        icone: 'https://i.ibb.co/LZYBg8Q/logotipo-circular-do-facebook.png'
+        nome: 'LinkedIn',
+        link: 'http://www.linkedin.com',
+        icone: 'https://i.ibb.co/12YfRcr/linkedin.png'
       },
       {
         nome: 'Instagram',
         link: 'http://www.instagram.com',
         icone: 'https://i.ibb.co/0nNbcgS/instagram-1.png'
+      },{
+        nome: 'Facebook',
+        link: 'http://www.facebook.com',
+        icone: 'https://i.ibb.co/LZYBg8Q/logotipo-circular-do-facebook.png'
       },
-      {
-        nome: 'LinkedIn',
-        link: 'http://www.linkedin.com',
-        icone: 'https://i.ibb.co/12YfRcr/linkedin.png'
-      }
     ]
 }
